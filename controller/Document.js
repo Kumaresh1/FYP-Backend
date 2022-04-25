a8a54b2bd4f6a14717e25cdeeea1bf116dd4712d
Kumaresh <kumareshappu12@gmail.com>
2022-04-25 22:10:42 +0530

    fam member

--- controller/Document.js
+++ controller/Document.js
@@ -172,14 +172,44 @@ exports.ReadDocumentByPhone = function (req, res, next) {
 };
 
 function parseDate(str) {
-  var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
-  return m ? new Date(m[3], m[2] - 1, m[1]) : null;
+  var m = str.match(
+    /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+  );
+  return m;
 }
 
+function getDate(d) {
+  var day, month, year;
+
+  let result = d.match("[0-9]{2}([-/ .])[0-9]{2}[-/ .][0-9]{4}");
+  if (null != result) {
+    let dateSplitted = result[0].split(result[1]);
+    day = dateSplitted[0];
+    month = dateSplitted[1];
+    year = dateSplitted[2];
+  }
+  result = d.match("[0-9]{4}([-/ .])[0-9]{2}[-/ .][0-9]{2}");
+  if (null != result) {
+    let dateSplitted = result[0].split(result[1]);
+    day = dateSplitted[2];
+    month = dateSplitted[1];
+    year = dateSplitted[0];
+  }
+
+  if (month > 12) {
+    aux = day;
+    day = month;
+    month = aux;
+  }
+
+  return year + "/" + month + "/" + day;
+}
 exports.OcrToJson = function (req, res, next) {
   //console.log(req.query, req.params);
   const ocr = req.body.ocr;
-  let findDates = parseDate(ocr);
+
+  let findDates = getDate(ocr);
+  console.log(findDates);
   return res.status(200).json({
     dates: findDates,
   });
